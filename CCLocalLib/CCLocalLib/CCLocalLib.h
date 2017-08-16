05b8ed05efb3ff287f5439b0974a9fec8e353424
ElwinFrederick <elwinfrederick@163.com>
2017-08-16 10:28:24 +0800

    allow protocol cc valued . add "CCLocalLib.h" .

---
+++ CCLocalLib/CCLocalLib/CCLocalLib.h
@@ -0,0 +1,19 @@
+//
+//  CCLocalLib.h
+//  CCLocalLibrary
+//
+//  Created by Elwinfrederick on 16/08/2017.
+//  Copyright © 2017 冯明庆. All rights reserved.
+//
+
+#ifndef CCLocalLib_h
+#define CCLocalLib_h
+
+    #import "NSObject+CCProtocol.h"
+
+    #import "CCCommonViewTools.h"
+    #import "CCCommonDefine.h"
+    #import "CCCommonTools.h"
+    #import "CCCommonDataTools.h"
+
+#endif /* CCLocalLib_h */
---
+++ CCLocalLib/CCLocalLib/CCProtocol/NSObject+CCProtocol.m
@@ -0,0 +1,17 @@
+//
+//  NSObject+CCProtocol.m
+//  RisSubModule
+//
+//  Created by Elwinfrederick on 16/08/2017.
+//  Copyright © 2017 ElwinFrederick. All rights reserved.
+//
+
+#import "NSObject+CCProtocol.h"
+
+@implementation NSObject (CCProtocol)
+
+- (instancetype)cc {
+    return self;
+}
+
+@end
