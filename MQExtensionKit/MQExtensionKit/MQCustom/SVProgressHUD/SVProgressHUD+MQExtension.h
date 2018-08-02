09ac1e84957b0ef2d6ee588551c66753b8d6a700
Elwin Frederick <elwinfrederick@163.com>
2018-08-02 11:02:18 +0800

    1. added `SVProgressHUD+MQExtension` to CCCustom .

---
+++ MQExtensionKit/MQExtensionKit/MQCustom/SVProgressHUD/SVProgressHUD+MQExtension.h
@@ -0,0 +1,31 @@
+//
+//  SVProgressHUD+MQExtension.h
+//  MQExtension_Example
+//
+//  Created by ElwinFrederick on 2018/8/1.
+//  Copyright © 2018 ElwinFrederick. All rights reserved.
+//
+
+#if __has_include(<SVProgressHUD/SVProgressHUD.h>)
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wstrict-prototypes"
+    @import SVProgressHUD;
+#pragma clang diagnostic pop
+
+@interface SVProgressHUD (MQExtension)
+
+/// adding notification for SVProgressHUD using blocks . // 使用 block 给 SVProgressHUD 添加通知
++ (void) mq_notification : (void(^)(NSNotification *sender ,
+                                    NSString * s_notification_name ,
+                                    id user_info)) mq_notification_block
+                 for_key : (NSString *) s_key ;
+
+/// be sure to remove the block when dealloc (like normal notifications) .
+// 一定记得 在 dealloc 的时候 remove 掉 这个 . (像普通的通知一样)
++ (void) mq_notification_remove_for_key : (NSString *) s_key ;
++ (void) mq_notification_remove_all ; // 移除所有的通知
+
+@end
+
+#endif
---
+++ MQExtensionKit/MQExtensionKit/MQCustom/SVProgressHUD/SVProgressHUD+MQExtension.m
@@ -0,0 +1,128 @@
+//
+//  SVProgressHUD+MQExtension.m
+//  MQExtension_Example
+//
+//  Created by ElwinFrederick on 2018/8/1.
+//  Copyright © 2018 ElwinFrederick. All rights reserved.
+//
+
+#import "SVProgressHUD+MQExtension.h"
+
+@interface SVProgressHUD (MQExtension_Assist)
+
+@property (nonatomic , class , readonly) NSMutableDictionary < NSString * , void(^)(NSNotification * , NSString * , id) > *dictionary_notification_block ;
+
++ (void) mq_add_notification ;
++ (void) mq_remove_notification ;
++ (void) mq_handle_notification : (NSNotification *) sender ;
+
+@end
+
+@implementation SVProgressHUD (MQExtension_Assist)
+
+static NSMutableDictionary *__d_notification_block = nil;
+
++ (NSMutableDictionary<NSString *,void (^)(NSNotification *, NSString * , id)> *)dictionary_notification_block {
+    if (__d_notification_block) return __d_notification_block;
+    NSMutableDictionary *t = [NSMutableDictionary dictionary];
+    __d_notification_block = t;
+    return __d_notification_block;
+}
+
++ (void) mq_add_notification {
+    NSNotificationCenter *notification_center = [NSNotificationCenter defaultCenter];
+    [notification_center addObserver:self
+                            selector:@selector(mq_handle_notification:)
+                                name:SVProgressHUDWillAppearNotification
+                              object:nil];
+    [notification_center addObserver:self
+                            selector:@selector(mq_handle_notification:)
+                                name:SVProgressHUDDidAppearNotification
+                              object:nil];
+    
+    [notification_center addObserver:self
+                            selector:@selector(mq_handle_notification:)
+                                name:SVProgressHUDWillDisappearNotification
+                              object:nil];
+    [notification_center addObserver:self
+                            selector:@selector(mq_handle_notification:)
+                                name:SVProgressHUDDidDisappearNotification
+                              object:nil];
+    
+    [notification_center addObserver:self
+                            selector:@selector(mq_handle_notification:)
+                                name:SVProgressHUDDidTouchDownInsideNotification
+                              object:nil];
+    [notification_center addObserver:self
+                            selector:@selector(mq_handle_notification:)
+                                name:SVProgressHUDDidReceiveTouchEventNotification
+                              object:nil];
+}
+
++ (void) mq_remove_notification {
+    NSNotificationCenter *notification_center = [NSNotificationCenter defaultCenter];
+    [notification_center removeObserver:self
+                                   name:SVProgressHUDWillAppearNotification
+                                 object:nil];
+    [notification_center removeObserver:self
+                                   name:SVProgressHUDDidAppearNotification
+                                 object:nil];
+    
+    [notification_center removeObserver:self
+                                   name:SVProgressHUDWillDisappearNotification
+                                 object:nil];
+    [notification_center removeObserver:self
+                                   name:SVProgressHUDDidDisappearNotification
+                                 object:nil];
+    
+    [notification_center removeObserver:self
+                                   name:SVProgressHUDDidTouchDownInsideNotification
+                                 object:nil];
+    [notification_center removeObserver:self
+                                   name:SVProgressHUDDidReceiveTouchEventNotification
+                                 object:nil];
+}
+
++ (void) mq_handle_notification : (NSNotification *) sender {
+    [self.dictionary_notification_block.allValues enumerateObjectsUsingBlock:^(void (^ _Nonnull obj)(NSNotification *, NSString *, id), NSUInteger idx, BOOL * _Nonnull stop) {
+        if (obj) obj(sender , sender.name , sender.userInfo[SVProgressHUDStatusUserInfoKey]);
+    }];
+}
+
+@end
+
+#pragma mark - -----
+
+@implementation SVProgressHUD (MQExtension)
+
+static BOOL __SVProgressHUD_is_added_notification = false ;
+
++ (void) mq_notification : (void(^)(NSNotification *sender ,
+                                    NSString * s_notification_name,
+                                    id user_info)) mq_notification_block
+                 for_key : (NSString *) s_key {
+    
+    if (!__SVProgressHUD_is_added_notification) {
+        [self mq_add_notification];
+        __SVProgressHUD_is_added_notification = YES;
+    }
+    
+    if (mq_notification_block && s_key) {
+        [self.dictionary_notification_block setValue:mq_notification_block
+                                              forKey:s_key];
+    }
+    
+}
+
++ (void) mq_notification_remove_for_key : (NSString *) s_key {
+    if (s_key && [self.dictionary_notification_block valueForKey:s_key]) {
+        [self.dictionary_notification_block removeObjectForKey:s_key];
+    }
+}
++ (void) mq_notification_remove_all {
+    [self.dictionary_notification_block removeAllObjects];
+    [self mq_remove_notification];
+    __SVProgressHUD_is_added_notification = false;
+}
+
+@end
