63ad66816df429ed77fad661e229106006223b6b
Elwin Frederick <elwinfrederick@163.com>
2018-09-04 18:16:53 +0800

    1. update with new calculate method for UILabel 's height .

--- MQExtensionKit/MQExtensionKit/MQView/UILabel/UILabel+MQExtension.h
+++ MQExtensionKit/MQExtensionKit/MQView/UILabel/UILabel+MQExtension.h
@@ -15,11 +15,14 @@
 /// auto fit with text || attributed Text , // 自适应 文本 , 富文本高度
 /// params fEstimate that determins if the height after calculate ,  was lesser than original . // 参数 fEstimate 决定了计算高度后 , 是否比原来的高度小
 /// note: ignores text-indent , attributed text's level will be higher than others // 无视缩进 , 富文本优先级高于其他 .
-- (instancetype) mq_auto_height : (CGFloat) fEstimate ;
-- (instancetype) mq_attributed_text_height : (CGFloat) fEstimate ;
-- (instancetype) mq_text_height : (CGFloat) fEstimate ;
+- (instancetype) mq_auto_height : (CGFloat) f_estimate ;
+- (instancetype) mq_attributed_text_height : (CGFloat) f_estimate ;
+- (instancetype) mq_text_height : (CGFloat) f_estimate ;
 
 /// using sizeThatFits:(CGSize) to calculate the height . // 使用 sizeThatFits:(CGSize) 去计算高度
-- (instancetype) mq_attributed_text_height_t : (CGFloat)fEstimate ;
+- (CGSize) mq_attributed_text_height : (CGFloat) f_estimate
+                               style : ( NSParagraphStyle * _Nonnull ) style
+                                font : ( UIFont * _Nonnull ) font
+                                text : ( NSString * _Nonnull ) s_text ;
 
 @end
--- MQExtensionKit/MQExtensionKit/MQView/UILabel/UILabel+MQExtension.m
+++ MQExtensionKit/MQExtensionKit/MQView/UILabel/UILabel+MQExtension.m
@@ -21,32 +21,52 @@
     return label;
 }
 
-- (instancetype) mq_auto_height : (CGFloat) fEstimate {
+- (instancetype) mq_auto_height : (CGFloat) f_estimate {
     if (self.attributedText
-        && self.attributedText.length) return [self mq_attributed_text_height:fEstimate];
-    if (self.text && self.text.length) return [self mq_text_height:fEstimate];
+        && self.attributedText.length) return [self mq_attributed_text_height:f_estimate];
+    if (self.text && self.text.length) return [self mq_text_height:f_estimate];
     return self;
 }
-- (instancetype) mq_attributed_text_height : (CGFloat) fEstimate {
+- (instancetype) mq_attributed_text_height : (CGFloat) f_estimate {
     self.height = MQ_TEXT_HEIGHT_A(self.width,
-                                   fEstimate,
+                                   f_estimate,
                                    self.attributedText);
     return self;
 }
-- (instancetype) mq_text_height : (CGFloat) fEstimate {
+- (instancetype) mq_text_height : (CGFloat) f_estimate {
     self.height = MQ_TEXT_HEIGHT_C(self.width,
-                                   fEstimate,
+                                   f_estimate,
                                    self.text,
                                    self.font,
                                    self.lineBreakMode);
     return self;
 }
 
-- (instancetype) mq_attributed_text_height_t : (CGFloat)fEstimate {
+- (CGSize) mq_attributed_text_height : (CGFloat) f_estimate
+                               style : ( NSParagraphStyle * _Nonnull ) style
+                                font : ( UIFont * _Nonnull ) font
+                                text : ( NSString * _Nonnull ) s_text ; {
+    
     CGSize size = [self sizeThatFits:CGSizeMake(self.width, MAXFLOAT)];
-    if (size.height > fEstimate) self.height = size.height;
-    else self.height = fEstimate;
-    return self;
+    
+    BOOL (^cc_is_contains_chinese)(NSString *) = ^BOOL (NSString *s) {
+        NSString *s_t = [NSString stringWithFormat:@"%@",s];
+        for(int i = 0 ; i < s_t.length ; i++){
+            int t = [s_t characterAtIndex:i];
+            if( t > 0x4e00 && t < 0x9fff) return YES ;
+        }
+        return false;
+    };
+    
+    NSInteger i_space = size.height - font.lineHeight;
+    if (i_space <= style.lineSpacing) {
+        if (cc_is_contains_chinese(s_text)) {
+            CGFloat f_height = size.height - style.lineSpacing ;
+            return (CGSize){size.width,(f_height > f_estimate ? f_height : f_estimate)};
+        }
+    }
+    
+    return (CGSize){size.width,(size.height > f_estimate ? size.height : f_estimate)};
 }
 
 @end
